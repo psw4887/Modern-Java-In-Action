e866854d19c4c8f7d92ecd309e76ac9dce3cf703
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-06-28 10:19:49 +0900

    98P 작성중

--- src/main/java/com/nhnacademy/lambda/LambdaExpression.java
+++ src/main/java/com/nhnacademy/lambda/LambdaExpression.java
@@ -1,18 +1,24 @@
 package com.nhnacademy.lambda;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LambdaExpression {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         LambdaExpression le = new LambdaExpression();
 
         // TODO 1: 람다, 익명클래스를 사용한 예
         le.run((n) -> log.trace("lambda 사용"), 1);
 
-        // TODO 2: 
+        // TODO 2: 실행 어라운드 패턴
+        log.trace(le.processFile());
+
+
 
     }
 
@@ -20,10 +26,25 @@ public class LambdaExpression {
 
         p.print(type);
     }
+
+    private String processFile() throws IOException {
+        try(BufferedReader bf = new BufferedReader(new FileReader("data.txt"))) {
+            return bf.readLine();
+        }
+    }
+
+    private String processFile(BufferReaderProcessor b) throws IOException {
+
+    }
 }
 
 @FunctionalInterface
 interface Print <T> {
 
     void print(T type);
+}
+
+@FunctionalInterface
+interface  BufferReaderProcessor {
+    String process(BufferedReader b) throws IOException;
 }
